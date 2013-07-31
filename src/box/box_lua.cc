e9f14b1983c40cba7116740306e45e9660c9e3db
Roman Tsisyk <roman@tsisyk.com>
2013-07-31 12:42:32 +0400

    Update pushrequest code to use new request structure

--- src/box/box_lua.cc
+++ src/box/box_lua.cc
@@ -1742,289 +1742,88 @@ lbox_on_request_skip(struct lua_State *L)
 	return 0;
 }
 
+static int
+lbox_request_push_key(struct lua_State *L, const char **keypos,
+		      const char *keyend, uint32_t key_part_count)
+{
+	lua_newtable(L);
+	for (uint32_t p = 0; p < key_part_count; p++) {
+		uint32_t part_len;
+		const char *part = pick_field_str(keypos, keyend, &part_len);
+		lua_pushnumber(L, p + 1);
+		lua_pushlstring(L, part, part_len);
+		lua_settable(L, -3);
+	}
+	return 1;
+}
 
 static int
 lbox_request_index(struct lua_State *L)
 {
-	const char *s;
-	uint32_t l, i, j, count, fcount;
-
 	lua_pushstring(L, "raw");
 	lua_gettable(L, 1);
 	struct request *request = (struct request *) lua_touserdata(L, -1);
 	lua_pop(L, 1);
 
-	typeof(request->data) data	= request->data;
-	const char **reqpos		= &data;
-	const char *reqend		= request->data + request->len;
-
 	const char *key = lua_tostring(L, 2);
-	if (strcmp(key, "type") == 0) {
+	if (strcmp(key, "key") == 0) {
+		const char *keypos;
+		uint32_t key_part_count;
 		switch(request->type) {
-			case REPLACE:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "replace");
-				lua_rawset(L, 1);
-				break;
-			case DELETE_1_3:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "delete13");
-				lua_rawset(L, 1);
-				break;
-			case DELETE:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "delete");
-				lua_rawset(L, 1);
-				break;
-			case SELECT:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "select");
-				lua_rawset(L, 1);
-				break;
-			case UPDATE:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "update");
-				lua_rawset(L, 1);
-				break;
-			case CALL:
-				lua_pushvalue(L, 2);
-				lua_pushstring(L, "call");
-				lua_rawset(L, 1);
-				break;
-			default:
-				luaL_error(L, "unknown request type: %d",
-					request->type);
-				break;
-		}
-	} else if (strcmp(key, "space") == 0) {
-		switch(request->type) {
-			case REPLACE:
-			case DELETE:
-			case DELETE_1_3:
-			case SELECT:
-			case UPDATE:
-				lua_pushvalue(L, 2);
-				lua_pushnumber(L, pick_u32(reqpos, reqend) );
-				lua_rawset(L, 1);
-				break;
-			default:
-				luaL_error(L,
-					"there is no 'space' in reqtype=%d",
-					request->type);
-		}
-	} else if (strcmp(key, "flags") == 0) {
-		switch(request->type) {
-			case REPLACE:
-			case DELETE:
-			case UPDATE:
-				pick_u32(reqpos, reqend);	/* spaceno */
-			case CALL:
-				lua_pushvalue(L, 2);
-				lua_pushnumber(L, pick_u32(reqpos, reqend));
-				lua_rawset(L, 1);
+		case SELECT:
+			if (request->s.key_count == 0)
 				break;
 
-			default:
-				break;
-		}
-	} else if (strcmp(key, "proc") == 0) {
-		if (request->type == CALL) {
-			pick_u32(reqpos, reqend);	/* flags */
-
-			s = pick_field_str(reqpos, reqend, &l);
-
-			/* proc name */
+			/* Extract the first key */
+			keypos = request->s.keys;
+			key_part_count = pick_u32(&keypos, request->s.keys_end);
 			lua_pushvalue(L, 2);
-			lua_pushlstring(L, s, l);
+			lbox_request_push_key(L, &keypos, request->s.keys_end,
+					      key_part_count);
 			lua_rawset(L, 1);
-		}
-	} else if (strcmp(key, "key") == 0) {
-		switch(request->type) {
-			case REPLACE:
-			case DELETE:
-			case UPDATE:
-			case DELETE_1_3:
-				pick_u32(reqpos, reqend);	  /* space */
-				if (request->type != DELETE_1_3)
-					pick_u32(reqpos, reqend); /* flags */
-
-				fcount = pick_u32(reqpos, reqend);
-
-				lua_pushvalue(L, 2);
-				lua_newtable(L);
-
-				for (i = 0; i < fcount; i++) {
-					s = pick_field_str(reqpos, reqend, &l);
-					lua_pushnumber(L, i + 1);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-				}
-				(void)i;
-				(void)fcount;
-
-
-				lua_rawset(L, 1);
-				break;
-
-			default:
-				break;
-		}
-	} else if (strcmp(key, "keys") == 0) {
-		if (request->type == SELECT) {
-			pick_u32(reqpos, reqend);	/* space */
-			pick_u32(reqpos, reqend);	/* index */
-			pick_u32(reqpos, reqend);	/* offset */
-			pick_u32(reqpos, reqend);	/* limit */
-
+			break;
+		case UPDATE:
+			keypos = request->u.key;
 			lua_pushvalue(L, 2);
-			lua_newtable(L);
-			count = pick_u32(reqpos, reqend);
-			for (i = 0; i < count; i++) {
-				lua_pushnumber(L, i + 1);
-				lua_newtable(L);
-				fcount = pick_u32(reqpos, reqend);
-				for (j = 0; j < fcount; j++) {
-					s = pick_field_str(reqpos, reqend, &l);
-					lua_pushnumber(L, j + 1);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-				}
-				lua_settable(L, -3);
-			}
+			lbox_request_push_key(L, &keypos, request->u.key_end,
+					      request->u.key_part_count);
 			lua_rawset(L, 1);
-		}
-	} else if (strcmp(key, "index") == 0) {
-		if (request->type == SELECT) {
-			pick_u32(reqpos, reqend);       /* space */
+			break;
+		case DELETE:
+		case DELETE_1_3:
+			keypos = request->d.key;
 			lua_pushvalue(L, 2);
-			lua_pushnumber(L, pick_u32(reqpos, reqend));
+			lbox_request_push_key(L, &request->d.key,
+				request->d.key_end, request->d.key_part_count);
 			lua_rawset(L, 1);
+			break;
 		}
-	} else if (strcmp(key, "offset") == 0) {
-		if (request->type == SELECT) {
-			pick_u32(reqpos, reqend);       /* space */
-			pick_u32(reqpos, reqend);       /* index */
-			lua_pushvalue(L, 2);
-			lua_pushnumber(L, pick_u32(reqpos, reqend));
-			lua_rawset(L, 1);
+	} else if (strcmp(key, "tuple") == 0 && request->type == REPLACE) {
+		const char *keypos = request->r.tuple;
+		uint32_t key_part_count = pick_u32(&keypos,
+						   request->r.tuple_end);
+		lua_pushvalue(L, 2);
+		lbox_request_push_key(L, &keypos, request->r.tuple_end,
+				      key_part_count);
+		lua_rawset(L, 1);
+	} else if (request->type == SELECT && strcmp(key, "keys") == 0) {
+		lua_pushvalue(L, 2);
+		lua_newtable(L);
+		const char *keypos = request->s.keys;
+		for (uint32_t k = 0; k < request->s.key_count; k++) {
+			lua_pushnumber(L, k + 1);
+			uint32_t key_part_count = pick_u32(&keypos,
+				request->s.keys_end);
+			lbox_request_push_key(L, &keypos,
+				request->s.keys_end, key_part_count);
+			lua_settable(L, -3);
 		}
-	} else if (strcmp(key, "limit") == 0) {
-		if (request->type == SELECT) {
-			pick_u32(reqpos, reqend);       /* space */
-			pick_u32(reqpos, reqend);       /* index */
-			pick_u32(reqpos, reqend);       /* offset */
-			lua_pushvalue(L, 2);
-			lua_pushnumber(L, pick_u32(reqpos, reqend));
-			lua_rawset(L, 1);
-		}
-	} else if (strcmp(key, "format") == 0 || strcmp(key, "opargs") == 0) {
-		if (request->type == UPDATE) {
-			pick_u32(reqpos, reqend);       /* space */
-			pick_u32(reqpos, reqend);	/* flags */
-
-			/* key */
-			fcount = pick_u32(reqpos, reqend);
-			for (i = 0; i < fcount; i++)
-				pick_field_str(reqpos, reqend, &l);
-
-
-			lua_pushstring(L, "opargs");
-			lua_newtable(L);
-
-			/* operations */
-			count = pick_u32(reqpos, reqend);
-
-			char *format = (char *) alloca(2 * count + 1);
-			format[2 * count] = 0;
-			for (j = i = 0; i < count; i++) {
-				uint32_t fno = pick_u32(reqpos, reqend);
-				uint8_t op = pick_u8(reqpos, reqend);
-
-				lua_pushnumber(L, ++j);
-				lua_pushnumber(L, fno);
-				lua_settable(L, -3);
-
-				if (op == 5) {
-					/* splice */
-					format[i * 2] = ':';
-					format[1 + i * 2] = 'p';
-
-					s = *reqpos;
-					l = pick_varint32(reqpos, reqend);
-					*reqpos += l;
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s,
-						((const char *)*reqpos) - s);
-					lua_settable(L, -3);
-					continue;
-				}
-
-				s = pick_field_str(reqpos, reqend, &l);
-
-
-				switch(op) {
-				case 0:	/* set */
-					format[i * 2] = '=';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-				case 1: /* add */
-					format[i * 2] = '+';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-				case 2: /* and */
-					format[i * 2] = '&';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-				case 3: /* xor */
-					format[i * 2] = '^';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-				case 4: /* or */
-					format[i * 2] = '|';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-				case 6: /* delete */
-					format[i * 2] = '#';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushstring(L, "");
-					lua_settable(L, -3);
-					break;
-				case 7: /* insert_before */
-					format[i * 2] = '!';
-					format[1 + i * 2] = 'p';
-					lua_pushnumber(L, ++j);
-					lua_pushlstring(L, s, l);
-					lua_settable(L, -3);
-					break;
-
-
-				default:
-					luaL_error(L, "unknown opcode: %d", op);
-				}
-			}
-
-			lua_rawset(L, 1);
-
-			lua_pushstring(L, "format");
-			lua_pushstring(L, format);
-			lua_rawset(L, 1);
-		}
+		lua_rawset(L, 1);
+	} else if (request->type == UPDATE && strcmp(key, "expr") == 0) {
+		lua_pushvalue(L, 2);
+		lua_pushlstring(L, request->u.expr,
+				request->u.expr_end - request->u.expr);
+		lua_rawset(L, 1);
 	}
 
 	lua_pushvalue(L, 2);
@@ -2035,6 +1834,7 @@ lbox_request_index(struct lua_State *L)
 static int
 lbox_push_request(struct lua_State *L, struct request *request)
 {
+	say_warn("__index");
 	lua_newtable(L);
 	lua_pushstring(L, "raw");
 	lua_pushlightuserdata(L, request);
@@ -2045,6 +1845,71 @@ lbox_push_request(struct lua_State *L, struct request *request)
 	lua_pushcclosure(L, lbox_request_index, 0);
 	lua_settable(L, -3);
 	lua_setmetatable(L, -2);
+
+	lua_pushstring(L, "type_id");
+	lua_pushinteger(L, request->type);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "flags");
+	lua_pushinteger(L, request->flags);
+	lua_settable(L, -3);
+
+	switch(request->type) {
+	case SELECT:
+		lua_pushstring(L, "type");
+		lua_pushstring(L, "SELECT");
+		lua_settable(L, -3);
+		lua_pushstring(L, "space_id");
+		lua_pushinteger(L, request->s.space_no);
+		lua_settable(L, -3);
+		lua_pushstring(L, "index_id");
+		lua_pushinteger(L, request->s.index_no);
+		lua_settable(L, -3);
+		lua_pushstring(L, "offset");
+		lua_pushinteger(L, request->s.offset);
+		lua_settable(L, -3);
+		lua_pushstring(L, "limit");
+		lua_pushinteger(L, request->s.limit);
+		lua_settable(L, -3);
+		break;
+	case REPLACE:
+		lua_pushstring(L, "type");
+		lua_pushstring(L, "REPLACE");
+		lua_settable(L, -3);
+		lua_pushstring(L, "space_id");
+		lua_pushinteger(L, request->r.space_no);
+		lua_settable(L, -3);
+		break;
+	case UPDATE:
+		lua_pushstring(L, "type");
+		lua_pushstring(L, "UPDATE");
+		lua_settable(L, -3);
+		lua_pushstring(L, "space_id");
+		lua_pushinteger(L, request->u.space_no);
+		lua_settable(L, -3);
+		break;
+	case DELETE:
+	case DELETE_1_3:
+		lua_pushstring(L, "type");
+		lua_pushstring(L, "DELETE");
+		lua_settable(L, -3);
+		lua_pushstring(L, "space_id");
+		lua_pushinteger(L, request->d.space_no);
+		lua_settable(L, -3);
+		break;
+	case CALL:
+		lua_pushstring(L, "type");
+		lua_pushstring(L, "CALL");
+		lua_settable(L, -3);
+		lua_pushstring(L, "procname");
+		lua_pushlstring(L, request->c.procname,
+				request->c.procname_len);
+		lua_settable(L, -3);
+		break;
+	default:
+		assert(false);
+	}
+
 	return 1;
 }
 
--- test/box/on_request.test.py
+++ test/box/on_request.test.py
@@ -28,7 +28,7 @@ admin("box.insert(26, 2, 2, 3)")
 admin("box.replace(27, 2, 2, 3)")
 
 admin("box.remove_on_request(id)")
-admin("id = box.on_request(function(req, skip) return req.space end)")
+admin("id = box.on_request(function(req, skip) return req.space_id end)")
 admin("box.select(23, 12, 1)")
 admin("box.delete(24, 12, 1)")
 admin("box.update(25, 12, '=p', 1, 2)")
@@ -50,6 +50,9 @@ admin("box.select(23, 12, 1)")
 admin("box.delete(24, 12, 1)")
 admin("box.update(25, 12, '=p', 1, 2)")
 admin("box.insert(26, 2, 2, 3)")
+
+admin("box.remove_on_request(id)")
+admin("id = box.on_request(function(req, skip) return req.tuple end)")
 admin("box.replace(27, 2, 2, 3)")
 
 admin("box.remove_on_request(id)")
@@ -57,7 +60,7 @@ admin("id = box.on_request(function(req, skip) return unpack(req.keys) end)")
 admin("box.select(23, 12, 1)")
 
 admin("box.remove_on_request(id)")
-admin("id = box.on_request(function(req, skip) return req.index end)")
+admin("id = box.on_request(function(req, skip) return req.index_id end)")
 admin("box.select(23, 12, 1)")
 
 admin("box.remove_on_request(id)")
@@ -69,10 +72,9 @@ admin("id = box.on_request(function(req, skip) return req.limit end)")
 admin("box.select(23, 12, 1)")
 
 admin("box.remove_on_request(id)")
-admin("id = box.on_request(function(req, skip) return req.format end)")
+admin("id = box.on_request(function(req, skip) return req.expr end)")
 admin("box.update(23, 12, '=p!p#p', 11, 2, 12, 3, 34, 4)")
 
 admin("box.remove_on_request(id)")
-admin("id = box.on_request(function(req, skip) return req.opargs end)")
+admin("id = box.on_request(function(req, skip) return req.expr end)")
 admin("box.update(23, 12, '=p!p#p&p:p', 11, 2, 12, 3, 34, 4, 22, 21, 27, box.pack('ppp', 223, 3332, 'abc'))")
-
