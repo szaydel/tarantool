1921f0c912bd96820585511b5d5d3cfc3118391a
knizhnik <knizhnik@garret.ru>
2014-07-03 16:41:45 +0400

    Fix field ids from 0-based to 1-based in box/trans tests

--- test/box/trans.test.lua
+++ test/box/trans.test.lua
@@ -2,26 +2,26 @@ space = box.schema.create_space('account')
 space:create_index('primary', { type = 'hash' })
 for i = 1,100,1 do space:insert({i, 100}) end
 --# setopt delimiter ';'
-for i = 1,99,1 do 
+for i = 1,99,1 do
     space:begin()
-    space:update({i}, {{'-', 1, 50}})
-    space:update({i+1}, {{'+', 1, 50}})
+    space:update({i}, {{'-', 2, 50}})
+    space:update({i+1}, {{'+', 2, 50}})
     space:commit()
 end;
 --# setopt delimiter ''
 space:select{100}
 
 --# setopt delimiter ';'
-for i = 1,1,1 do 
+for i = 1,1,1 do
     space:begin()
-    space:update({100}, {{'-', 1, 100}})
+    space:update({100}, {{'-', 2, 100}})
     space:rollback()
 end;
 --# setopt delimiter ''
 space:select{100}
- 
+
 space:begin()
-space:update({100}, {{'+', 1, 100}})
+space:update({100}, {{'+', 2, 100}})
 space:rollback()
 
 space:drop()
--- test/box/trans_box.test.lua
+++ test/box/trans_box.test.lua
@@ -4,8 +4,8 @@ for i = 1,100,1 do space:insert({i, 100}) end
 --# setopt delimiter ';'
 for i = 1,99,1 do
     space:begin()
-    space:update({i}, {{'-', 1, 50}})
-    space:update({i+1}, {{'+', 1, 50}})
+    space:update({i}, {{'-', 2, 50}})
+    space:update({i+1}, {{'+', 2, 50}})
     space:commit()
 end;
 --# setopt delimiter ''
@@ -14,7 +14,7 @@ space:select{100}
 --# setopt delimiter ';'
 for i = 1,1,1 do
     space:begin()
-    space:update({100}, {{'-', 1, 100}})
+    space:update({100}, {{'-', 2, 100}})
     space:rollback()
 end;
 --# setopt delimiter ''
